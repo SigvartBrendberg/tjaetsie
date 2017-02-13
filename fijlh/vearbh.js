975d389a315e960c0a67bf66c30d0fa4a3417fbf
SigvartBrendberg <walterhohman@gmail.com>
2017-02-13 14:11:24 +0100

    vearbh script

--- fijlh/vearbh.js
+++ fijlh/vearbh.js
@@ -1,9 +1,38 @@
 var vearbh = [
-[],/*ulike*/
-[],/*1*/
-[],/*2*/
-[],/*3*/
-[],/*4*/
-[],/*5*/
-[] /*6*/
-]
+["Guvviedidh","Vaedtsedh","Dåeriedidh","Byöpmedidh","Murhkestidh"],/*golme-lihtsh*/
+["Jiehtedh","Maehtedh"],/*1*/
+["Laavkodh","Darjodh"],/*2*/
+["Damtedh","Gaahkedh","Guarkedh"],/*3*/
+["Edtjedh","Gossedh","Goetsedh"],/*4*/
+["RANDOM_ERROR"],/*5*/
+["Garmelostedh","Geesniedh","Geevledh","Gihtjedh"] /*6*/
+];
+
+var daelie;
+var baakoe = document.getElementById("gyhtjelasse");
+
+var orre = function(){
+	daelie = Math.floor(Math.random()*7);
+	baakoe.innerHTML =
+	vearbh[daelie][
+		Math.floor(
+			Math.random() * vearbh[daelie].length
+		)
+	];
+	for(var i=0;i<7;i++){
+		document.getElementById(i).style.background = "#8080F0";
+	};
+}
+
+var aelkedh = function(){
+	orre();
+};
+
+var vearbhClick = function(gusnie){
+	if(gusnie === daelie){
+		orre();
+	}
+	else{
+		document.getElementById(gusnie).style.background = "#FF8080";
+	};
+};
