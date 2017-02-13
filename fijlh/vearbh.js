bafeaf15c9f8b2cbe4d0b2a6b7b369c3b517baa7
SigvartBrendberg <walterhohman@gmail.com>
2017-02-13 15:32:25 +0100

    Add files via upload

--- fijlh/vearbh.js
+++ fijlh/vearbh.js
@@ -4,12 +4,17 @@ var vearbh = [
 ["Laavkodh","Darjodh"],/*2*/
 ["Damtedh","Gaahkedh","Guarkedh"],/*3*/
 ["Edtjedh","Gossedh","Goetsedh"],/*4*/
-["RANDOM_ERROR"],/*5*/
+["Dongkedh","Byøredh"],/*5*/
 ["Garmelostedh","Geesniedh","Geevledh","Gihtjedh"] /*6*/
 ];
 
+var ding = new Audio('../media/complete.ogg');
+var isRunning = false;
+
 var daelie;
+var poeng = 0;
 var baakoe = document.getElementById("gyhtjelasse");
+var poengDisplay = document.getElementById("poeng");
 
 var orre = function(){
 	daelie = Math.floor(Math.random()*7);
@@ -26,13 +31,19 @@ var orre = function(){
 
 var aelkedh = function(){
 	orre();
+	isRunning = true;
 };
 
 var vearbhClick = function(gusnie){
 	if(gusnie === daelie){
 		orre();
+		poeng += 2;
+		poengDisplay.innerHTML = poeng;
+		ding.play();
 	}
-	else{
+	else if(isRunning){
 		document.getElementById(gusnie).style.background = "#FF8080";
+		poeng--;
+		poengDisplay.innerHTML = poeng;
 	};
 };
